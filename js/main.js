bc065e4fb146e1eb06a12176b4f21d335e21d006
Minori Kikugawa <minocolor@gmail.com>
2022-07-05 14:11:25 +0900

    ScrollTriggerのレスポンシブ対応

--- js/main.js
+++ js/main.js
@@ -1,47 +1,80 @@
-
-ScrollTrigger.create({
-  trigger: ".fixed-screen",
-  start: "top 30%",
-  end: "bottom top",
-  markers: true,
-  endTrigger: "#footer",
-  pin: true,
-  pinSpacing: true,
+ScrollTrigger.matchMedia({
+  "(min-width:641px)": function () {
+    gsap.to(".fixed-screen", {
+      scrollTrigger: {
+        trigger: ".fixed-screen",
+        start: "top 100px",
+        end: "bottom 50%",
+        markers: true,
+        endTrigger: "#about",
+        pin: true,
+        pinSpacing: true,
+      }
+    })
+  }
 })
-
-ScrollTrigger.create({
-  trigger: ".info-item-1",
-  start: "top 100px",
-  markers: true,
-  animation: gsap.to(".screen-item-1", { opacity: 1 }),
-  toggleActions: "play none none reverse"
+ScrollTrigger.matchMedia({
+  "(min-width:641px)": function () {
+    gsap.to(".info-item-1", {
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: ".info-item-1",
+        start: "top center",
+        markers: false,
+        toggleActions: "play none none reverse"
+      }
+    })
+  }
 })
-ScrollTrigger.create({
-  trigger: ".info-item-2",
-  start: "top 100px",
-  markers: true,
-  animation: gsap.to(".screen-item-2", { opacity: 1 }),
-  toggleActions: "play none none reverse"
+ScrollTrigger.matchMedia({
+  "(min-width:641px)": function () {
+    gsap.to(".screen-item-2", {
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: ".info-item-2",
+        start: "top center",
+        markers: false,
+        toggleActions: "play none none reverse"
+      }
+    })
+  }
 })
-ScrollTrigger.create({
-  trigger: ".info-item-3",
-  start: "top 100px",
-  markers: true,
-  animation: gsap.to(".screen-item-3", { opacity: 1 }),
-  toggleActions: "play none none reverse"
+ScrollTrigger.matchMedia({
+  "(min-width:641px)": function () {
+    gsap.to(".screen-item-3", {
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: ".info-item-3",
+        start: "top center",
+        markers: false,
+        toggleActions: "play none none reverse"
+      }
+    })
+  }
 })
-ScrollTrigger.create({
-  trigger: ".info-item-4",
-  start: "top 100px",
-  markers: true,
-  animation: gsap.to(".screen-item-4", { opacity: 1 }),
-  toggleActions: "play none none reverse"
+ScrollTrigger.matchMedia({
+  "(min-width:641px)": function () {
+    gsap.to(".screen-item-4", {
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: ".info-item-4",
+        start: "top center",
+        markers: false,
+        toggleActions: "play none none reverse"
+      }
+    })
+  }
 })
-ScrollTrigger.create({
-  trigger: ".info-item-5",
-  start: "top 100px",
-  markers: true,
-  animation: gsap.to(".screen-item-5", { opacity: 1 }),
-  toggleActions: "play none none reverse"
+ScrollTrigger.matchMedia({
+  "(min-width:641px)": function () {
+    gsap.to(".screen-item-5", {
+      autoAlpha: 1,
+      scrollTrigger: {
+        trigger: ".info-item-5",
+        start: "top center",
+        markers: false,
+        toggleActions: "play none none reverse"
+      }
+    })
+  }
 })
-
