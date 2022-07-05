6afe1caf162a5263913dc5c504d2b4d076955726
Minori Kikugawa <minocolor@gmail.com>
2022-07-05 18:27:24 +0900

    js リファクタリング

--- js/main.js
+++ js/main.js
@@ -1,80 +1,46 @@
-ScrollTrigger.matchMedia({
-  "(min-width:641px)": function () {
-    gsap.to(".fixed-screen", {
-      scrollTrigger: {
-        trigger: ".fixed-screen",
-        start: "top 100px",
-        end: "bottom 50%",
-        markers: true,
-        endTrigger: "#about",
-        pin: true,
-        pinSpacing: true,
-      }
-    })
-  }
-})
-ScrollTrigger.matchMedia({
-  "(min-width:641px)": function () {
-    gsap.to(".info-item-1", {
-      autoAlpha: 1,
-      scrollTrigger: {
-        trigger: ".info-item-1",
-        start: "top center",
-        markers: false,
-        toggleActions: "play none none reverse"
-      }
-    })
-  }
-})
-ScrollTrigger.matchMedia({
-  "(min-width:641px)": function () {
-    gsap.to(".screen-item-2", {
-      autoAlpha: 1,
-      scrollTrigger: {
-        trigger: ".info-item-2",
-        start: "top center",
-        markers: false,
-        toggleActions: "play none none reverse"
-      }
-    })
-  }
-})
-ScrollTrigger.matchMedia({
-  "(min-width:641px)": function () {
-    gsap.to(".screen-item-3", {
-      autoAlpha: 1,
-      scrollTrigger: {
-        trigger: ".info-item-3",
-        start: "top center",
-        markers: false,
-        toggleActions: "play none none reverse"
-      }
-    })
-  }
-})
-ScrollTrigger.matchMedia({
-  "(min-width:641px)": function () {
-    gsap.to(".screen-item-4", {
-      autoAlpha: 1,
-      scrollTrigger: {
-        trigger: ".info-item-4",
-        start: "top center",
-        markers: false,
-        toggleActions: "play none none reverse"
-      }
-    })
-  }
-})
-ScrollTrigger.matchMedia({
-  "(min-width:641px)": function () {
-    gsap.to(".screen-item-5", {
-      autoAlpha: 1,
-      scrollTrigger: {
-        trigger: ".info-item-5",
-        start: "top center",
-        markers: false,
-        toggleActions: "play none none reverse"
-      }
-    })
-  }
-})
+const stArray = [
+  {
+    name: '.fixed-screen',
+    params: { trigger: '.fixed-screen', start: 'top 100px', end: 'bottom 50%', markers: false, endTrigger: '#about', pin: true, pinSpacing: true, toggleActions: 'play none none reverse' }
+  },
+  {
+    name: '.screen-item-1',
+    params: { autoAlpha: 1, trigger: '.info-item-1', start: 'top center', markers: false, toggleActions: 'play none none reverse' }
+  },
+  {
+    name: '.screen-item-2',
+    params: { autoAlpha: 1, trigger: '.info-item-2', start: 'top center', markers: false, toggleActions: 'play none none reverse' }
+  },
+  {
+    name: '.screen-item-3',
+    params: { autoAlpha: 1, trigger: '.info-item-3', start: 'top center', markers: false, toggleActions: 'play none none reverse' }
+  },
+  {
+    name: '.screen-item-4',
+    params: { autoAlpha: 1, trigger: '.info-item-4', start: 'top center', markers: false, toggleActions: 'play none none reverse' }
+  },
+  {
+    name: '.screen-item-5',
+    params: { autoAlpha: 1, trigger: '.info-item-5', start: 'top center', markers: false, toggleActions: 'play none none reverse' }
+  },
+]
+// console.log(stArray)
+stArray.forEach((obj) =>
+  ScrollTrigger.matchMedia({
+    "(min-width:641px)": function () {
+      gsap.to(obj.name, {
+        autoAlpha: obj.params.autoAlpha,
+        scrollTrigger: {
+          trigger: obj.params.trigger,
+          start: obj.params.start,
+          end: obj.params.end,
+          endTrigger: obj.params.endTrigger,
+          pin: obj.params.pin,
+          pinSpacing: obj.params.pinSpacing,
+          markers: obj.params.markers,
+          toggleActions: obj.params.toggleActions
+        }
+      })
+    }
+  })
+)
